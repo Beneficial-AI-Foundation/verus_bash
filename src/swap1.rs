4ab6254817ced2048103dd91267b431dba34ef9b
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-08-04 10:31:05 +0100

    Shorten filename

--- src/swap1.rs
+++ src/swap1.rs
@@ -7,8 +7,8 @@ verus! {
 pub fn swap(file1: &str, file2: &str, fs: &mut HashMap<String, Vec<u8>>) -> (result: Result<(), MvError>)
     requires
         file1 != file2,
-        file2 != "temp_swap_file",
-        file1 != "temp_swap_file"
+        file2 != "tmp_file",
+        file1 != "tmp_file"
     ensures
         match result {
             Ok(()) => {
@@ -16,8 +16,8 @@ pub fn swap(file1: &str, file2: &str, fs: &mut HashMap<String, Vec<u8>>) -> (res
                 (get_file(&old(fs), file1).is_some() && get_file(&old(fs), file2).is_some()) ==> (
                     get_file(fs, file1) == get_file(&old(fs), file2) &&
                     get_file(fs, file2) == get_file(&old(fs), file1) &&
-                    get_file(fs, "temp_swap_file").is_none() &&
-                    forall|k: &str| k != file1 && k != file2 && k != "temp_swap_file" ==>
+                    get_file(fs, "tmp_file").is_none() &&
+                    forall|k: &str| k != file1 && k != file2 && k != "tmp_file" ==>
                         get_file(fs, k) == get_file(&old(fs), k)
                 ) &&
                 // Otherwise, filesystem remains unchanged
@@ -26,7 +26,7 @@ pub fn swap(file1: &str, file2: &str, fs: &mut HashMap<String, Vec<u8>>) -> (res
                 )
             },
             Err(MvError) => {
-                    forall|k: &str| k != file1 && k != file2 && k != "temp_swap_file" ==>
+                    forall|k: &str| k != file1 && k != file2 && k != "tmp_file" ==>
                         get_file(fs, k) == get_file(&old(fs), k)
             }
         }
@@ -36,9 +36,9 @@ pub fn swap(file1: &str, file2: &str, fs: &mut HashMap<String, Vec<u8>>) -> (res
     
     if file1_exists && file2_exists {
         // Both files exist - swap them using a temporary name
-        mv(file1, "temp_swap_file", fs)?;
+        mv(file1, "tmp_file", fs)?;
         mv(file2, file1, fs)?;
-        mv("temp_swap_file", file2, fs)?;
+        mv("tmp_file", file2, fs)?;
     }
     // Otherwise, do nothing
     Ok(())
