64646c4345cbbffb781ee897e2946a630c63bf5e
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-08-04 10:12:59 +0100

    Claude shortens the function calls

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,15 @@ use std::collections::HashMap;
 use vstd::prelude::*;
 
 mod lib;
+use lib::*;
 
 verus! {
 
 fn main() {
     let mut fs = HashMap::new();
     
-    if lib::test("foo", &fs) {
-        lib::mv("foo", "bar", &mut fs);
+    if test("foo", &fs) {
+        mv("foo", "bar", &mut fs);
     }
 }
 
