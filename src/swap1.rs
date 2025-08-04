395ed04e28b6ac055f00ca65eb6993e7f5cc34ea
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-08-04 10:19:41 +0100

    Tell Claude to fix the spec

--- src/swap1.rs
+++ src/swap1.rs
@@ -13,22 +13,8 @@ pub fn swap(file1: &str, file2: &str, fs: &mut HashMap<String, Vec<u8>>)
             forall|k: &str| k != file1 && k != file2 ==>
                 get_file(fs, k) == get_file(&old(fs), k)
         ),
-        // If only file1 existed, it becomes file2 and file1 doesn't exist
-        (get_file(&old(fs), file1).is_some() && get_file(&old(fs), file2).is_none()) ==> (
-            get_file(fs, file2) == get_file(&old(fs), file1) &&
-            get_file(fs, file1).is_none() &&
-            forall|k: &str| k != file1 && k != file2 ==>
-                get_file(fs, k) == get_file(&old(fs), k)
-        ),
-        // If only file2 existed, it becomes file1 and file2 doesn't exist
-        (get_file(&old(fs), file1).is_none() && get_file(&old(fs), file2).is_some()) ==> (
-            get_file(fs, file1) == get_file(&old(fs), file2) &&
-            get_file(fs, file2).is_none() &&
-            forall|k: &str| k != file1 && k != file2 ==>
-                get_file(fs, k) == get_file(&old(fs), k)
-        ),
-        // If neither file existed, filesystem remains unchanged
-        (get_file(&old(fs), file1).is_none() && get_file(&old(fs), file2).is_none()) ==> (
+        // Otherwise, filesystem remains unchanged
+        (get_file(&old(fs), file1).is_none() || get_file(&old(fs), file2).is_none()) ==> (
             *fs == old(fs)
         )
 {
@@ -40,14 +26,8 @@ pub fn swap(file1: &str, file2: &str, fs: &mut HashMap<String, Vec<u8>>)
         let _ = mv(file1, "temp_swap_file", fs);
         let _ = mv(file2, file1, fs);
         let _ = mv("temp_swap_file", file2, fs);
-    } else if file1_exists {
-        // Only file1 exists - move it to file2
-        let _ = mv(file1, file2, fs);
-    } else if file2_exists {
-        // Only file2 exists - move it to file1
-        let _ = mv(file2, file1, fs);
     }
-    // If neither exists, do nothing
+    // Otherwise, do nothing
 }
 
 }
